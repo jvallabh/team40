8b4abbc4baa055582b037650f9bce50c24e91fcf
Saket Adusumilli <saketadu@buffalo.edu>
2014-02-20 16:30:43 -0500

    Changes so that Projector works perfect

--- src/edu/buffalo/cse562/ProjectionOperator.java
+++ src/edu/buffalo/cse562/ProjectionOperator.java
@@ -6,7 +6,13 @@ package edu.buffalo.cse562;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.buffalo.cse562.Evaluator.Type;
+
+import net.sf.jsqlparser.expression.BinaryExpression;
+import net.sf.jsqlparser.expression.DoubleValue;
 import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
+import net.sf.jsqlparser.statement.create.table.ColDataType;
 import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.select.AllColumns;
 import net.sf.jsqlparser.statement.select.SelectExpressionItem;
@@ -26,11 +32,13 @@ public class ProjectionOperator implements Operator {
 	ColumnDefinition[] schema;
 	List<SelectItem> selectItems;
 	ArrayList<Integer> itemList = new ArrayList<>();
+	ColumnDefinition[] finalSchema;
 	
 	public ProjectionOperator(Operator input, ColumnDefinition[] schema, List<SelectItem> selectItems) {
 		this.input = input;
 		this.schema = schema;
 		this.selectItems = selectItems;
+		this.finalSchema = changeSchema(selectItems);
 	}
 	
 	public int getColumnID(Column col) {
@@ -40,6 +48,40 @@ public class ProjectionOperator implements Operator {
 			}
 		}
 		return -1;
+	}
+	
+	public ColDataType getNumType(Expression expr) {
+		if (expr instanceof BinaryExpression) {
+			ColDataType colDataType = getNumType(((BinaryExpression) expr).getLeftExpression());
+			if (colDataType == null) {
+				return getNumType(((BinaryExpression) expr).getRightExpression());
+			}
+			return colDataType;	
+		}
+		else if (expr instanceof Column) {
+			Column col = (Column) expr;
+			return (schema[getColumnID(col)].getColDataType());
+		}
+		return null;
+	}
+	
+	public ColumnDefinition[] changeSchema(List selectItems) {
+		ColumnDefinition[] schema = new ColumnDefinition[selectItems.size()];
+		SelectExpressionItem exp;
+		for (int j=0; j<selectItems.size(); j++) {
+			exp = (SelectExpressionItem)selectItems.get(j);
+			if(exp.getAlias()!=null) {
+				schema[j] = new ColumnDefinition();
+				schema[j].setColumnName(exp.getAlias());
+			}
+			else {
+				schema[j] = new ColumnDefinition();
+				schema[j].setColumnName(exp.toString());
+			}
+			schema[j].setColDataType(getNumType(exp.getExpression()));
+		}
+		
+		return schema;
 	}
 	
 	@Override
@@ -74,6 +116,6 @@ public class ProjectionOperator implements Operator {
 	@Override
 	public ColumnDefinition[] getSchema() {
 		// TODO Auto-generated method stub
-		return schema;
+		return finalSchema;
 	}
 }
