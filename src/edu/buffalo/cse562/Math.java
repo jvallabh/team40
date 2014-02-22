8c6698dd1264904d7f5108a10b54170d1d19cb2d
Jagadeesh <jagadeesh@Archie.(none)>
2014-02-22 17:43:35 -0500

    Change filnal x

--- src/edu/buffalo/cse562/Math.java
+++ src/edu/buffalo/cse562/Math.java
@@ -20,11 +20,13 @@ public class Math {
 			ans = d ;
 		else
 			ans = c;
+		
 		if(schema[i].colDef.getColDataType().getDataType().equals("double"))
 			return Double.toString(ans);
 		else
 			return Integer.toString((int)ans);	
 	}
+	
 	public static String getMax(String a , String b, int i, ColumnInfo[] schema){
 		double c = Double.parseDouble(a);
 		double d = Double.parseDouble(b);
@@ -34,51 +36,25 @@ public class Math {
 			ans = c ;
 		else
 			ans = d;
+		
 		if(schema[i].colDef.getColDataType().getDataType().equals("double"))
 			return Double.toString(ans);
 		else
 			return Integer.toString((int)ans);
 		
 	}
+	
 	public static String getSum(String a , String b, int i, ColumnInfo[] schema){
 		double c = Double.parseDouble(a);
 		double d = Double.parseDouble(b);
 		
 		if(schema[i].colDef.getColDataType().getDataType().equalsIgnoreCase("double"))
 			return Double.toString(c+d);
+		else if(schema[i].colDef.getColDataType().getDataType().equalsIgnoreCase("decimal"))
+			return Double.toString(c+d);
 		else
-			return Integer.toString((int)(c+d));	
-		
-			
+			return Integer.toString((int)c+(int)d);		
 	}
-	public static Type getDataType( Column expr, ColumnInfo[] schema) {
-		
-		
-			Column col = (Column) expr;
-			int colID;
-			// Gets the corresponding columnID from the schema
-			for (colID=0; colID<schema.length; colID++) {
-				if (schema[colID].colDef.getColumnName().equals(col.getColumnName())) {
-					break;
-				}
-			}
-			// Sets the data-type of the element in the column
-			if (schema[colID].colDef.getColDataType().getDataType().equals("int")) {
-				return Type.INT;
-			} else if (schema[colID].colDef.getColDataType().getDataType().equals("double")) {
-				return Type.DOUBLE;
-			} else if (schema[colID].colDef.getColDataType().getDataType().equals("string")) {
-				return Type.STRING;
-			} else if (schema[colID].colDef.getColDataType().getDataType().equals("date")) {
-				return Type.DATE;
-			}
-			return Type.DOUBLE;
-			// Sets the data-type of the element	
-		
-	}
-
-	
-	
 	
 	public static Type getNumType(Expression expr, ColumnInfo[] schema) {
 		if (expr instanceof BinaryExpression) {
@@ -106,52 +82,6 @@ public class Math {
 			return Type.DOUBLE;
 		}
 		return Type.DOUBLE;
-	}
-	
-	public static Type getDataType(Expression left, Expression right,ColumnInfo[] schema) {
-		Expression expr;
-		if (left instanceof Column) {
-			expr = left;
-		} else {
-			expr = right;
-		}
-		if (right instanceof BinaryExpression) {
-			if (left instanceof BinaryExpression) {
-				return getNumType(((BinaryExpression)left).getLeftExpression(),schema);
-			} else {
-				expr = left;
-			}
-		}
-		if (expr instanceof Column) {
-			Column col = (Column) expr;
-			int colID;
-			// Gets the corresponding columnID from the schema
-			for (colID=0; colID<schema.length; colID++) {
-				if (schema[colID].colDef.getColumnName().equals(col.getColumnName())) {
-					break;
-				}
-			}
-			// Sets the data-type of the element in the column
-			if (schema[colID].colDef.getColDataType().getDataType().equals("int")) {
-				return Type.INT;
-			} else if (schema[colID].colDef.getColDataType().getDataType().equals("double")) {
-				return Type.DOUBLE;
-			} else if (schema[colID].colDef.getColDataType().getDataType().equals("string")) {
-				return Type.STRING;
-			} else if (schema[colID].colDef.getColDataType().getDataType().equals("date")) {
-				return Type.DATE;
-			}
-			// Sets the data-type of the element
-		} else if (expr instanceof LongValue) {
-			return Type.INT;
-		} else if (expr instanceof DoubleValue) {
-			return Type.DOUBLE;
-		} else if (expr instanceof StringValue) {
-			return Type.STRING;
-		} else if (expr instanceof DateValue) {
-			return Type.DATE;
-		}
-		return Type.STRING;
 	}
 
 }
