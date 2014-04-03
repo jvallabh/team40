3860409c6de43919764a307402880ed076309f3f
anil <anil@anil-Inspiron-1525.(none)>
2014-04-02 21:39:54 -0400

    HashJoin Commit

--- src/edu/buffalo/cse562/Datum.java
+++ src/edu/buffalo/cse562/Datum.java
@@ -45,4 +45,30 @@ public class Datum {
 		return null;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((element == null) ? 0 : element.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Datum other = (Datum) obj;
+		if (element == null) {
+			if (other.element != null)
+				return false;
+		} else if (!element.equals(other.element))
+			return false;
+		return true;
+	}
+	
+
 }
