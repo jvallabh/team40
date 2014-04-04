6c676dee308d6283ecc5100f987a525fb3c9fa57
Praveen Bellam <pbellam@buffalo.edu>
2014-04-04 17:37:31 -0400

    Included a few datatype comparisions

--- src/edu/buffalo/cse562/SortableTuple.java
+++ src/edu/buffalo/cse562/SortableTuple.java
@@ -1,6 +1,8 @@
 package edu.buffalo.cse562;
 import java.util.Comparator;
 
+import com.sun.org.apache.xerces.internal.impl.dv.xs.DecimalDV;
+
 public class SortableTuple implements Comparator<SortableTuple> {
 	
 	Datum[] tuple;
@@ -24,16 +26,17 @@ public class SortableTuple implements Comparator<SortableTuple> {
 			if(colDataType.equalsIgnoreCase("double")){
 				x  = orderIndex[j]*Double.compare(Double.parseDouble(arg0.tuple[i].element),Double.parseDouble(arg1.tuple[i].element));
 			}				 
-			if(colDataType.equalsIgnoreCase("int")){
+			else if(colDataType.equalsIgnoreCase("DECIMAL")){
+			   x  =orderIndex[j]*Double.compare(Double.parseDouble(arg0.tuple[i].element),Double.parseDouble(arg1.tuple[i].element));
+			   //arg0.tuple[i].toString().compareTo(arg1.tuple[i].element);
+		    }				 
+			else if(colDataType.equalsIgnoreCase("int")){
 				x  = orderIndex[j]*Integer.compare(Integer.parseInt(arg0.tuple[i].element),Integer.parseInt(arg1.tuple[i].element));
 			}				 
-			if(colDataType.equalsIgnoreCase("double")){
+			else if(colDataType.equalsIgnoreCase("string") || colDataType.equalsIgnoreCase("VARCHAR") || colDataType.equalsIgnoreCase("CHAR")){
 				x  =orderIndex[j]*arg0.tuple[i].toString().compareTo(arg1.tuple[i].element);
 			}				 
-			if(colDataType.equalsIgnoreCase("string") || colDataType.equalsIgnoreCase("VARCHAR")){
-				x  =orderIndex[j]*arg0.tuple[i].toString().compareTo(arg1.tuple[i].element);
-			}				 
-			if(colDataType.equalsIgnoreCase("date")){
+			else if(colDataType.equalsIgnoreCase("date")){
 				x  =orderIndex[j]*arg0.tuple[i].Date().compareTo(arg1.tuple[i].Date());
 			}				 
 			j++;
