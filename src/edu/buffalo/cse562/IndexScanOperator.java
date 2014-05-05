d813c7a57af49899e4c9bc79200b69ef21dad97b
Saket Adusumilli <saketadu@buffalo.edu>
2014-05-05 02:51:23 -0400

    Changes to INdexScan

--- src/edu/buffalo/cse562/IndexScanOperator.java
+++ src/edu/buffalo/cse562/IndexScanOperator.java
@@ -102,7 +102,17 @@ public class IndexScanOperator implements Operator {
 				tableName = this.schema[index].tableName;
 			where = getIndexInTree(index, indexType, firstIndexCondition);
 			if(firstIndexCondition instanceof MinorThanEquals && !(((MinorThanEquals)firstIndexCondition).getRightExpression() instanceof Column)){
-				whereKey = iterList[where+1];
+				((MinorThanEquals)firstIndexCondition).getRightExpression().accept(eval);
+				whereKey = eval.getValue();
+				try{
+				if(tree.find(whereKey)==null){
+					where = getIndexInTree2(index, indexType, firstIndexCondition);
+					whereKey = iterList[where+1];
+				}
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
 				int columnId = eval.getColumnID(((Column)((MinorThanEquals)indexCondition).getLeftExpression()));
 				Expression secondIndexCondition = getGreaterThanWhereCondition(columnId);
 				if(secondIndexCondition != null){
@@ -115,6 +125,7 @@ public class IndexScanOperator implements Operator {
 				}
 			}
 			else if(firstIndexCondition instanceof MinorThan && !(((MinorThan)firstIndexCondition).getRightExpression() instanceof Column)){
+				where = getIndexInTree2(index, indexType, firstIndexCondition);
 					whereKey = iterList[where];
 					int columnId = eval.getColumnID(((Column)((MinorThan)indexCondition).getLeftExpression()));
 					Expression secondIndexCondition = getGreaterThanWhereCondition(columnId);
@@ -168,6 +179,43 @@ public class IndexScanOperator implements Operator {
 		String tableName;
 		try {;
 			tree = indexFile.treeMap(this.schema[schemaIndex].tableName+"_"+this.schema[schemaIndex].colDef.getColumnName());
+			//iterList = new String[tree.size()];
+			//tree.keySet().toArray(iterList);
+			if(indexType == 0){
+				((EqualsTo)condition_).getRightExpression().accept(eval);
+				indexInTree = new ArrayList(tree.keySet()).indexOf(eval.getValue());
+			}
+			else if(indexType == 1){
+				((MinorThan)condition_).getRightExpression().accept(eval);
+				indexInTree = searchKey(eval.getValue());
+			}
+			else if(indexType == 2){
+				((MinorThanEquals)condition_).getRightExpression().accept(eval);
+				indexInTree = searchKey(eval.getValue());
+			}
+			else if(indexType == 3){
+				((GreaterThan)condition_).getRightExpression().accept(eval);
+				indexInTree = searchKey(eval.getValue());
+				indexInTree++;
+			}
+			else{
+				((GreaterThanEquals)condition_).getRightExpression().accept(eval);
+				indexInTree = searchKey(eval.getValue());
+			}
+				
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return indexInTree;
+		
+	}
+	
+	private int getIndexInTree2(int schemaIndex, int indexType, Expression condition_){
+		int indexInTree = -1;
+		String tableName;
+		try {;
+			tree = indexFile.treeMap(this.schema[schemaIndex].tableName+"_"+this.schema[schemaIndex].colDef.getColumnName());
 			iterList = new String[tree.size()];
 			tree.keySet().toArray(iterList);
 			if(indexType == 0){
@@ -199,7 +247,6 @@ public class IndexScanOperator implements Operator {
 		return indexInTree;
 		
 	}
-	
 	
 	@Override
 	public Datum[] readOneTuple() {
