abb51ebfe893c7d4f5b59321041cfba2fcc4810f
Jagadeesh <jvallabh@buffalo.edu>
2015-03-22 21:57:28 -0400

    DB Project

--- src/edu/buffalo/cse562/GenerateStatistics.java
+++ src/edu/buffalo/cse562/GenerateStatistics.java
@@ -27,6 +27,7 @@ public class GenerateStatistics {
 	int numOperators;
 	ArrayList<Expression> whereCondExpressions;
 	ArrayList<Expression> conditionsOnSingleTables;
+	ArrayList<Datum[]> tupleList[];
 	
 	public GenerateStatistics(PlainSelect pselect, ArrayList[] partitionedConditions) {
 		whereCondExpressions = partitionedConditions[0];
@@ -37,28 +38,33 @@ public class GenerateStatistics {
 		pselect.getFromItem().accept(fromscan);
 		Operator firstTableOperator = fromscan.source;
 		
-		if(firstTableOperator instanceof ScanOperator){
-			((ScanOperator)firstTableOperator).conditions = Util.getConditionsOfTable(firstTableOperator.getSchema(), conditionsOnSingleTables);
+		if(firstTableOperator instanceof IndexScanOperator){
+			((IndexScanOperator)firstTableOperator).conditions = Util.getConditionsOfTable(firstTableOperator.getSchema(), conditionsOnSingleTables);
 		}
 
 		List<Join> joinDetails = pselect.getJoins();
 		numOperators = joinDetails == null?0:joinDetails.size();
 		tableOperators = new Operator[numOperators+1];
 		filteredSize = new int[numOperators+1];
+		tupleList = new ArrayList[numOperators+1];
+		for (int i=0; i<numOperators+1; i++) {
+			tupleList[i] = new ArrayList<Datum[]>();
+		}
 		
 		tableOperators[curNum] = firstTableOperator;
-		filteredSize[curNum] = getFilteredSize(tableOperators[curNum]);
+		//filteredSize[curNum] = getFilteredSize(tableOperators[curNum]);
 		curNum++;
 		if (numOperators > 0) {
 			for(Join currJoin:joinDetails){
 				FromScanner tempFromScan = new FromScanner(Main.dataDir, Main.tables);
 				currJoin.getRightItem().accept(tempFromScan);
 				tableOperators[curNum] = tempFromScan.source;
-				((ScanOperator)tableOperators[curNum]).conditions = Util.getConditionsOfTable(tableOperators[curNum].getSchema(), conditionsOnSingleTables);
-				filteredSize[curNum] = getFilteredSize(tableOperators[curNum]);
+				((IndexScanOperator)tableOperators[curNum]).conditions = Util.getConditionsOfTable(tableOperators[curNum].getSchema(), conditionsOnSingleTables);
+				//filteredSize[curNum] = getFilteredSize(tableOperators[curNum]);
 				curNum++;
 			}
 		}
+		getFilteredSize();
 		sortOperatorsOnSize();
 		reorderOperators();
 	}
@@ -66,6 +72,7 @@ public class GenerateStatistics {
 	public void reorderOperators() {
 		int tempSize;
 		Operator tempOperator;
+		ArrayList<Datum[]> tempList;
 		for (int i=1; i<numOperators-1; i++) {
 			int j = i+1;
 			while ((!hasCommonCondition(i)) && j!=numOperators) {
@@ -75,12 +82,15 @@ public class GenerateStatistics {
 				tempOperator = tableOperators[j];
 				tableOperators[j] = tableOperators[i];
 				tableOperators[i] = tempOperator;
+				tempList = tupleList[j];
+				tupleList[j] = tupleList[i];
+				tupleList[i] = tempList;
 				j++;
 			}
 		}
 
 		System.out.println("After reordering");
-		for (int i=0; i<numOperators; i++) {
+		for (int i=0; i<numOperators+1; i++) {
 			System.out.println(filteredSize[i]);
 		}
 	}
@@ -118,13 +128,14 @@ public class GenerateStatistics {
 	public void sortOperatorsOnSize() {
 		int tempSize;
 		Operator tempOperator;
+		ArrayList<Datum[]> tempList;
 		System.out.println("Before sort");
-		for (int i=0; i<numOperators; i++) {
+		for (int i=0; i<numOperators+1; i++) {
 			System.out.println(filteredSize[i]);
 		}
 		
-		for (int i=0; i<numOperators; i++) {
-			for (int j=0; j<numOperators-i-1; j++) {
+		for (int i=0; i<numOperators+1; i++) {
+			for (int j=0; j<numOperators-i; j++) {
 				if (filteredSize[j] > filteredSize[j+1]) {
 					tempSize = filteredSize[j+1];
 					filteredSize[j+1] = filteredSize[j];
@@ -132,22 +143,29 @@ public class GenerateStatistics {
 					tempOperator = tableOperators[j+1];
 					tableOperators[j+1] = tableOperators[j];
 					tableOperators[j] = tempOperator;
+					tempList = tupleList[i+1];
+					tupleList[i+1] = tupleList[i];
+					tupleList[i] = tempList;
 				}
 			}
 		}
 		System.out.println("After sort");
-		for (int i=0; i<numOperators; i++) {
+		for (int i=0; i<numOperators+1; i++) {
 			System.out.println(filteredSize[i]);
 		}
 	}
 	
-	public int getFilteredSize(Operator operator) {
-		int count = 0;
-		Datum[] tuple = null;
-		do {
-			tuple = operator.readOneTuple();
-			count++;
-		} while (tuple != null);
-		return count--;
+	public void getFilteredSize() {
+		for (int i=0; i<numOperators+1; i++) {
+			int count = 0;
+			Datum[] tuple = null;
+			tuple = tableOperators[i].readOneTuple();
+			while (tuple != null) {
+				tupleList[i].add(tuple);
+				count++;
+				tuple = tableOperators[i].readOneTuple();
+			} 
+			filteredSize[i] = count;
+		}
 	}
 }
